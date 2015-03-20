d8222d4276f2ac88600580cac502427a0270b755
Ashish Jain <ashishj@codeaurora.org>
2015-03-20 14:52:09 +0530

    ASoC: msm: qdsp6v2: add support for ALAC/APE/VORBIS
    This is squash of following changes:
    
    change: I22b7cf38684250d2f8d6f9aefcd43452bb18e7f9
    change: Ic5e937879579785dd1efa9ca8a14201b2b4b2e77
    change: Ie374209007b14538837fb961b6a4b9e13519857f
    change: Icae98e8b8bd79048165493dc413f335c42420acd
    Change: I39d50fdb9c2e3e9d78ac5c6573f9f93431aca918
    
    Change-Id: I1aabacb9b0e5b19e03f09b9f0a133f6e08cf9d57
    Signed-off-by: Sachin Mohan Gadag <sgadag@codeaurora.org>
    
    Conflicts:
    	include/sound/apr_audio-v2.h
    	include/sound/q6asm-v2.h
    	include/uapi/sound/compress_params.h
    	sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
    	sound/soc/msm/qdsp6v2/q6asm.c

--- include/sound/q6asm-v2.h
+++ include/sound/q6asm-v2.h
@@ -45,6 +45,10 @@
 #define FORMAT_AC3          0x0013
 #define FORMAT_EAC3         0x0014
 #define FORMAT_MP2          0x0015
+#define FORMAT_FLAC         0x0016
+#define FORMAT_ALAC         0x0017
+#define FORMAT_VORBIS       0x0018
+#define FORMAT_APE          0x0019
 
 #define ENCDEC_SBCBITRATE   0x0001
 #define ENCDEC_IMMEDIATE_DECODE 0x0002
@@ -360,6 +364,15 @@ int q6asm_media_format_block_wmapro(struct audio_client *ac,
 
 int q6asm_media_format_block_amrwbplus(struct audio_client *ac,
 			struct asm_amrwbplus_cfg *cfg);
+
+int q6asm_media_format_block_alac(struct audio_client *ac,
+			struct asm_alac_cfg *cfg, int stream_id);
+
+int q6asm_stream_media_format_block_vorbis(struct audio_client *ac,
+			struct asm_vorbis_cfg *cfg, int stream_id);
+
+int q6asm_media_format_block_ape(struct audio_client *ac,
+			struct asm_ape_cfg *cfg, int stream_id);
 
 int q6asm_ds1_set_endp_params(struct audio_client *ac,
 				int param_id, int param_value);
